fada09adbc2da3cba5d9fce560189b13a65fa6ba
xiaoqianghou <2662651103@qq.com>
2017-03-21 00:01:59 +0800

    Chapter7_3

--- app/src/main/java/org/xuxiaoxiao/mycriminalintent/CrimeFragment.java
+++ app/src/main/java/org/xuxiaoxiao/mycriminalintent/CrimeFragment.java
@@ -53,6 +53,7 @@ public class CrimeFragment extends Fragment {
         mDateButton = (Button)v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
         mDateButton.setEnabled(false);
+
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
